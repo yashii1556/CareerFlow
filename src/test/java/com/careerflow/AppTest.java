fff990c0ee5c17fbde1c95047c9c832f1e948927
Yashasvi Bisht <Yashii1556@icloud.com>
2026-09-18 11:18:06 +0530

    Initial CareerFlow application

---
+++ src/test/java/com/careerflow/AppTest.java
@@ -0,0 +1,13 @@
+package com.careerflow;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testApplication() {
+        assertTrue(true);
+    }
+}
